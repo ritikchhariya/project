52169dca35f74314e704c2e42f5007cb40b2c735
Ritik <ritikjaat28@gmail.com>
2023-09-14 20:42:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import chocolateData from './Data';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import Slider from 'react-slick'; // Import Slider component from react-slick
+import 'slick-carousel/slick/slick.css'; // Import slick-carousel CSS
+import 'slick-carousel/slick/slick-theme.css'; // Import slick-carousel theme CSS
 
 function App() {
+  const [selectedChocolates, setSelectedChocolates] = useState([]);
+
+  // Your existing code for handling chocolate selection and s
+  
+    const handleChocolateSelection = (chocolate, quantity) => {
+      const updatedSelection = [...selectedChocolates];
+      const existingIndex = updatedSelection.findIndex((c) => c.id === chocolate.id);
+  
+      if (existingIndex !== -1) {
+        updatedSelection[existingIndex].quantity = quantity;
+      } else {
+        updatedSelection.push({ ...chocolate, quantity });
+      }
+  
+      const totalItems = updatedSelection.reduce((total, c) => total + c.quantity, 0);
+  
+      if (totalItems <= 8) {
+        setSelectedChocolates(updatedSelection);
+      }
+    };
+  
+    const addChocolate = (chocolate) => {
+      handleChocolateSelection(chocolate, (selectedChocolates.find((c) => c.id === chocolate.id)?.quantity || 0) + 1);
+    };
+  
+    const subtractChocolate = (chocolate) => {
+      const currentQuantity = selectedChocolates.find((c) => c.id === chocolate.id)?.quantity || 0;
+      if (currentQuantity > 0) {
+        handleChocolateSelection(chocolate, currentQuantity - 1);
+      }
+    };
+  
+    const getTotalChocolatesAdded = (chocolateId) => {
+      const selectedChocolate = selectedChocolates.find((c) => c.id === chocolateId);
+      return selectedChocolate ? selectedChocolate.quantity : 0;
+    };
+  
+    const totalSelectedChocolates = selectedChocolates.reduce((total, c) => total + c.quantity, 0);
+  
+    const totalPrice = selectedChocolates.reduce((total, c) => total + c.price * c.quantity, 0);
+  
+    // Create an array of chocolateData grouped into arrays of 4
+    // const groupedChocolates = chocolateData.reduce((acc, _, index) => {
+    //   if (index % 4 === 0) {
+    //     acc.push(chocolateData.slice(index, index + 4));
+    //   }
+    //   return acc;
+    // }, []);
+  // Slider settings
+  const sliderSettings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4, // Number of cards to show at once (adjust as needed)
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200, // Breakpoint for medium-sized screens
+        settings: {
+          slidesToShow: 3, // Adjust the number of cards for medium screens
+        },
+      },
+      {
+        breakpoint: 992, // Breakpoint for small-sized screens
+        settings: {
+          slidesToShow: 2, // Adjust the number of cards for small screens
+        },
+      },
+      {
+        breakpoint: 768, // Breakpoint for extra small screens
+        settings: {
+          slidesToShow: 1, // Adjust the number of cards for extra small screens (mobile)
+        },
+      },
+    ],
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <div className="container">
+        <div className="head">
+        <h4 className='heading'>Total Chocolate: {totalSelectedChocolates}</h4>
+        <h4 className='heading2'>Total Price: ${totalPrice.toFixed(2)}</h4>
+        </div>
+        <Slider {...sliderSettings}>
+          {chocolateData.map((chocolate) => (
+            <div className="card bg-light" key={chocolate.id}>
+              <img src={chocolate.img} style={{ width: "100%", height: "50%" }} className="card-img-top" alt={chocolate.name} />
+              <div className="card-body">
+                <h5 className="card-title">{chocolate.name}</h5>
+                <p className="card-text">Price: ${chocolate.price.toFixed(2)}</p>
+                <span>Chocolates Added: {getTotalChocolatesAdded(chocolate.id)}</span>
+                <div>
+                <button className='btn btn-outline-danger m-2 ' onClick={() => addChocolate(chocolate)}>Add Chocolate</button>
+                <button className='btn btn-outline-danger m-1 ' onClick={() => subtractChocolate(chocolate)}>Subtract Chocolate</button>
+
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+   </>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,51 @@
+const chocolateData=[
+    {
+        id:1,
+        name:"Dairy Milk",
+        img:"https://images.unsplash.com/photo-1565071559227-20ab25b7685e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        price:3
+    },
+    {
+        id:2,
+        name:"Kit Kat",
+        img:"https://media.istockphoto.com/id/458090597/photo/kitkat-chocolate-candy-bar.jpg?s=2048x2048&w=is&k=20&c=svRRv8Cegihewurlp2L6C2yOuj0w-gCo6mWS7uGajOc=",
+        price:5
+    },
+    {
+        id:3,
+        name:"5 Star",
+        img:"https://img1.exportersindia.com/product_images/bc-small/2019/5/5118241/5-star-chocolate-1558517535-4919038.jpeg",
+        price:2
+    },
+    {
+        id:4,
+        name:"Snickers",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTmOTwkT_pqeTeaqDArzlABnKrS1S24rL2HA&usqp=CAU",
+        price:6
+    },
+    {
+        id:5,
+        name:"Munch",
+        img:"https://5.imimg.com/data5/SELLER/Default/2022/7/PT/ST/VR/16553030/munch-chocolate.png",
+        price:4
+    },
+    {
+        id:6,
+        name:"Bar One",
+        img:"https://mishry.com/wp-content/uploads/2021/06/nestle-bar-one-chocolate-review.jpg",
+        price:5
+    },
+    {
+        id:7,
+        name:"Dark Fantacy",
+        img:"https://bgstores.in/wp-content/uploads/2020/07/61hLMtTxIFL._SL1000_.jpg",
+        price:7
+    },
+    {
+        id:8,
+        name:"Hershey'S",
+        img:"https://www.bigbasket.com/media/uploads/p/l/40239142_1-hersheys-dark-chocolate-bar-rich-taste.jpg",
+        price:1
+    }
+]
+export default chocolateData
